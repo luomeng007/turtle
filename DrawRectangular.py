62c08825688dfb1da2e33466e8514d6e3d161d46
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-10-05 18:45:55 +0200

    Update DrawRectangular.py

--- DrawRectangular.py
+++ DrawRectangular.py
@@ -5,31 +5,34 @@ Created on Mon Oct  5 17:42:20 2020
 @author: 15025
 """
 
-import turtle
-# draw a rectangular
-# problem: why everytime we second time run this program, it will arouse an error:
-# Traceback (most recent call last):
-#   File "C:\documents\pythonProgram\python100Day\pictureDraw.py", line 5, in <module>
-#     turtle.pensize(4)    
+import turtle  
 
 
 class Picture:
     def drawRectangular(self):
+        # create turtle object
+        obj = turtle.Turtle()
         # set the width of pen
-        turtle.pensize(4)
+        obj.pensize(4)
         # set the color of pen
-        turtle.pencolor('red')
-        turtle.forward(100)
-        turtle.right(90)
-        turtle.forward(100)
-        turtle.right(90)
-        turtle.forward(100)
-        turtle.right(90)
-        turtle.forward(100)
+        obj.pencolor('red')
+        
+        obj.forward(100)
+        obj.right(90)     # Rotate clockwise by 90 degrees
+        
+        obj.forward(100)
+        obj.right(90)
+        
+        obj.forward(100)
+        obj.right(90)
+        
+        obj.forward(100)
+        obj.right(90)
+        
         # start draw
         # start event-loop, then we could close graphics through click close button of tkinter
         # turtle.done() is same as turtle.mainloop()
-        turtle.mainloop()        
+        turtle.done()        
         
 
 if __name__ == "__main__":
